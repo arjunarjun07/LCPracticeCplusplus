85ecd022dda28ebe106ea8442b4ff133f07f049d
Arjun N <arjun199522@gmail.com>
2023-04-14 15:30:23 +0530

    Heaps

--- Leet/Leet/Heap.cpp
+++ Leet/Leet/Heap.cpp
@@ -1,4 +1,7 @@
 #include "Heap.h"
+#include <algorithm>
+#include <queue>
+#include <map>
 
 MyHeap::MyHeap()
 {
@@ -44,6 +47,94 @@ int MyHeap::LEFT(int i, const vector<int> &list)
 	}
 
 	return -1;
+}
+
+int MyHeap::lastStoneWeight(vector<int>& stones)
+{
+	std::priority_queue<int> max_heap;
+
+	for (auto elem: stones)
+	{
+		max_heap.push(elem);
+	}
+
+	while (max_heap.size() > 1) 
+	{
+		auto first_greatest_stone = max_heap.top();
+		max_heap.pop();
+
+		auto second_greatest_stone = max_heap.top();
+		max_heap.pop();
+
+		auto diff_weights = std::abs(first_greatest_stone - second_greatest_stone);
+
+		if (diff_weights != 0)
+		{
+			max_heap.push(diff_weights);
+		}
+
+	}
+
+
+	int res = (max_heap.size() > 0) ? max_heap.top() : 0;
+
+	return res;
+}
+
+class myComparator
+{
+public:
+	bool operator() (const POINT& p1, const POINT& p2)
+	{
+		auto p1_dis = (pow(p1.x, 2) + pow(p1.y, 2));
+		auto p2_dis = (pow(p2.x, 2) + pow(p2.y, 2));
+
+		bool result = (p1_dis > p2_dis) ? true : false;
+
+		return result;
+	}
+};
+
+int MyHeap::cal_euclidian_distance(POINT p1, POINT p2)
+{
+	// Calculating distance
+	return (pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2)); //no need of sqrt - for this problem only
+}
+
+vector<vector<int>> MyHeap::kClosest(vector<vector<int>>& points, int k)
+{
+	// cal euclidian dist between the point & origin
+	// build min heap using priority queue
+	// pop k nodes from the priority queue
+
+	vector<vector<int>> result;
+
+	priority_queue<POINT, vector<POINT>, myComparator > min_heap;
+
+	POINT origin(0, 0);
+
+	for (auto each_point_in : points)
+	{
+		POINT p1(each_point_in[0], each_point_in[1]);
+
+		int euclidian_dist = cal_euclidian_distance(origin, p1);
+
+		min_heap.push(p1);
+	}
+
+	while (min_heap.size() > 0 && k-- > 0)
+	{
+		auto p1 = min_heap.top();
+		min_heap.pop();
+
+		vector<int> point_as_vector{ p1.x, p1.y };
+		
+		result.push_back(point_as_vector);
+
+		point_as_vector.clear();
+	}
+
+	return result;
 }
 
 void MyHeap::MaxifyHeap(vector<int> &list, int indx)
@@ -116,4 +207,36 @@ int MyHeap::PARENT(int i, const vector<int>& list)
 	return -1;
 }
 
+KthLargest::KthLargest(int k, vector<int>& nums)
+{
+	copy(nums.begin(), nums.end(), back_inserter(nums_heap));
+	kthLargest = k;
+
+	std::make_heap(nums_heap.begin(), nums_heap.end(), std::greater<>{});
+
+	while (nums_heap.size() > k)
+	{
+		pop_heap(nums_heap.begin(), nums_heap.end(), std::greater<>{});
+		nums_heap.pop_back();
+	}
+}
+
+int KthLargest::add(int val)
+{
+	int res_val = -1;
+
+	nums_heap.push_back(val);
+	push_heap(nums_heap.begin(), nums_heap.end(), std::greater<>{});
+
+	//Need to fetch kth largest from the heap
+	while (nums_heap.size() > kthLargest)
+	{
+		pop_heap(nums_heap.begin(), nums_heap.end(), std::greater<>{});
+		nums_heap.pop_back();
+	}
+
+	res_val = nums_heap[0];
+
+	return res_val;
+}
 
--- Leet/Leet/Heap.h
+++ Leet/Leet/Heap.h
@@ -9,6 +9,24 @@ enum class HEAPTYPE
 	Max,
 	Min
 };
+
+class POINT
+{
+public:
+
+	int x, y;
+
+	POINT(int x_val = 0, int y_val = 0) :x(x_val), y(y_val)
+	{
+
+	}
+
+	~POINT()
+	{
+
+	}
+};
+
 class MyHeap
 {
 public:
@@ -24,8 +42,24 @@ public:
 	void MaxifyHeap(vector<int> &list, int indx);
 	void MinifyHeap(vector<int> &list, int indx);
 
+	int lastStoneWeight(vector<int>& stones);
+
+	vector<vector<int>> kClosest(vector<vector<int>>& points, int k);
+
+	int cal_euclidian_distance(POINT p1, POINT p2);
+
 	~MyHeap();
 
 private:
 
+};
+
+class KthLargest {
+
+	int kthLargest;
+	vector<int> nums_heap;
+
+public:
+	KthLargest(int k, vector<int>& nums);
+	int add(int val);
 };
